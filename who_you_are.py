99b567fc1ad8fb152c1e123641443022b4bb34a3
Itzztamzid <tamzidgamer16@gmail.com>
2026-08-19 23:34:28 +0600

    Update who_you_are.py

--- who_you_are.py
+++ who_you_are.py
@@ -1,1 +1,5 @@
-print("https://drive.google.com/file/d/1cqKIHIT3zTlgelgN6ZDuzAsLc6DIY8SE/view?usp=drivesdk")
+import os,time
+time.sleep(1)
+green = "\033[32m"
+reset = "\033[0m"
+print(f"\n\n\n{green}   https://drive.google.com/file/d/1cqKIHIT3zTlgelgN6ZDuzAsLc6DIY8SE/view?usp=drivesdk   {reset}")
