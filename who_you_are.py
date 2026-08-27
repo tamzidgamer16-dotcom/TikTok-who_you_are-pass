33534afab1d44e2e4e2a60c7eff964ef9ebcebf6
Itzztamzid <tamzidgamer16@gmail.com>
2026-08-27 15:00:30 +0600

    Update who_you_are.py

--- who_you_are.py
+++ who_you_are.py
@@ -2,4 +2,4 @@ import os,time
 time.sleep(1)
 red = "\033[31m"
 reset = "\033[0m"
-print(f"\n\n\n{red}   https://drive.google.com/file/d/1cqKIHIT3zTlgelgN6ZDuzAsLc6DIY8SE/view?usp=drivesdk   {reset}\n\n\n")
+print(f"\n\n\n{red}   https://drive.google.com/file/d/10woV_b4cQW3iTD2WoOC-G3mh2ZLOuzw0/view?usp=drivesdk   {reset}\n\n\n")
